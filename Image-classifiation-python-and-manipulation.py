adc1a45f6328ba39d158899c58a5ca7c1bccfe3a
krishna kakade <krishnakakade77@gmail.com>
2019-08-27 22:27:57 +0530

    Update Image-classifiation-python-and-manipulation.py

--- Image-classifiation-python-and-manipulation.py
+++ Image-classifiation-python-and-manipulation.py
@@ -17,17 +17,9 @@ fashion_mnist=keras.datasets.fashion_mnist
 import os
 print(os.getcwd())
 
-
-
-
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
-
-
-
 class_names=['T-shirt/top','Trouser','Pullover','Dress','Coat','Sandal','Shirt','Sneaker','Ankle boot','Bag']
-
-
 
 
 train_images.shape
